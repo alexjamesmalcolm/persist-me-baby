c89c3c1d7e4e5bddfe680e9f4b22d429c4ee938d
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-04-30 16:52:21 -0400

    Initial commit

---
+++ src/main/java/com/alexjamesmalcolm/persistmebaby/PersistMeBabyApplication.java
@@ -0,0 +1,12 @@
+package com.alexjamesmalcolm.persistmebaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistMeBabyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistMeBabyApplication.class, args);
+	}
+}
