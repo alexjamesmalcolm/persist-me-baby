feb3aa1dba7cc9be9e70b3936c3676eef1f2537a
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-05-02 12:31:13 -0400

    Added C in Crud

--- src/main/java/com/alexjamesmalcolm/persistmebaby/RestController.java
+++ src/main/java/com/alexjamesmalcolm/persistmebaby/RestController.java
@@ -4,11 +4,22 @@ import javax.annotation.Resource;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RestController {
-	
+
 	@Resource
 	private CrudRepository<Message, Long> messageRepo;
+
+	@RequestMapping(path = "/messages")
+	private Message receiveAPostRequestOnMessages(@RequestParam String text) {
+		Message message = new Message(text);
+		message = messageRepo.save(message);
+		return message;
+	}
 	
+//	@RequestMapping(path = "/messages/{messageId}")
+
 }
