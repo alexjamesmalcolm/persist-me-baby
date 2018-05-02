11bed0aa088cd4e8e3f51b0f09d9237276378d76
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-05-02 12:38:43 -0400

    Forgot to actually add update to the restful api but here's delete instead

--- src/main/java/com/alexjamesmalcolm/persistmebaby/RestController.java
+++ src/main/java/com/alexjamesmalcolm/persistmebaby/RestController.java
@@ -1,5 +1,9 @@
 package com.alexjamesmalcolm.persistmebaby;
 
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 import javax.annotation.Resource;
 
 import org.springframework.data.repository.CrudRepository;
@@ -15,22 +19,27 @@ public class RestController {
 	@Resource
 	private CrudRepository<Message, Long> messageRepo;
 
-	@RequestMapping(path = "/messages", method = RequestMethod.POST)
+	@RequestMapping(path = "/messages", method = POST)
 	private Message receiveAPostRequestOnMessages(@RequestParam String text) {
 		Message message = new Message(text);
 		message = messageRepo.save(message);
 		return message;
 	}
 
-	@RequestMapping(path = "/messages/{messageId}", method = RequestMethod.GET)
+	@RequestMapping(path = "/messages/{messageId}", method = GET)
 	private Message receiveAGetRequestOnAMessage(@PathVariable long messageId) {
 		Message message = messageRepo.findOne(messageId);
 		return message;
 	}
 
-	@RequestMapping(path = "/messages", method = RequestMethod.GET)
+	@RequestMapping(path = "/messages", method = GET)
 	private Iterable<Message> receiveAGetRequestOnMessages() {
 		Iterable<Message> messages = messageRepo.findAll();
 		return messages;
+	}
+	
+	@RequestMapping(path = "/messages/{messageId}", method = DELETE)
+	private void receiveDeleteRequestOnMessage(long messageId) {
+		messageRepo.delete(messageId);
 	}
 }
