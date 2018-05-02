284d58452f8e0027841746d50d6f1db9a56c7689
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-05-02 12:34:03 -0400

    Added R in Crud

--- src/main/java/com/alexjamesmalcolm/persistmebaby/RestController.java
+++ src/main/java/com/alexjamesmalcolm/persistmebaby/RestController.java
@@ -4,7 +4,9 @@ import javax.annotation.Resource;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -13,13 +15,17 @@ public class RestController {
 	@Resource
 	private CrudRepository<Message, Long> messageRepo;
 
-	@RequestMapping(path = "/messages")
+	@RequestMapping(path = "/messages", method = RequestMethod.POST)
 	private Message receiveAPostRequestOnMessages(@RequestParam String text) {
 		Message message = new Message(text);
 		message = messageRepo.save(message);
 		return message;
 	}
-	
-//	@RequestMapping(path = "/messages/{messageId}")
+
+	@RequestMapping(path = "/messages/{messageId}", method = RequestMethod.GET)
+	private Message receiveAGetRequestOnAMessage(@PathVariable long messageId) {
+		Message message = messageRepo.findOne(messageId);
+		return message;
+	}
 
 }
