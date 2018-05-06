24108407ab950d3936169ef6bbc3b4e99bcab220
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-05-06 17:45:48 -0400

    The controller now displays the index template

--- src/main/java/com/alexjamesmalcolm/persistmebaby/MessageController.java
+++ src/main/java/com/alexjamesmalcolm/persistmebaby/MessageController.java
@@ -1,9 +1,13 @@
 package com.alexjamesmalcolm.persistmebaby;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MessageController {
-	
-//	public String display
+
+	@RequestMapping("/")
+	public String displayHome() {
+		return "index";
+	}
 }
