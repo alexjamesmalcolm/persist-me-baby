690bbb05af35f6c13fda2dddd3c5eb91c57b86a9
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-05-29 19:11:56 -0400

    Created SecurityController

---
+++ src/main/java/com/alexjamesmalcolm/persistmebaby/controller/SecurityController.java
@@ -0,0 +1,18 @@
+package com.alexjamesmalcolm.persistmebaby.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SecurityController {
+	
+	@RequestMapping(value = "/username", method = RequestMethod.GET)
+	@ResponseBody
+	public String currentUsername(Principal principal) {
+		return principal.getName();
+	}
+}
