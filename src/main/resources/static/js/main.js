2b309bec77df5ba764055e3b234ee106ae9769f7
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-05-29 15:12:27 -0400

    Refactored out common code among sendMessage and updateMessages

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,65 +1,66 @@
 const initialize = () => {
-    const button = document.querySelector("section.message-form button[type='submit']");
-    const textBox = document.querySelector("section.message-form input[type='text']");
-    button.addEventListener("click", () => {
-        sendMessage(textBox);
-    });
-    textBox.addEventListener("keyup", () => {
-        if (event.keyCode === 13) {
-            button.click();
-        }
-    });
-    setInterval(() => {
-        updateMessages();
-    }, 2000);
+	const button = document.querySelector("section.message-form button[type='submit']");
+	const textBox = document.querySelector("section.message-form input[type='text']");
+	button.addEventListener("click", () => {
+		sendMessage(textBox);
+	});
+	textBox.addEventListener("keyup", () => {
+		if (event.keyCode === 13) {
+			button.click();
+		}
+	});
+	setInterval(() => {
+		updateMessages();
+	}, 2000);
 };
 
-const sendMessage = (textBox) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState === 4 && xhr.status === 200) {
-            const messages = document.querySelector("section.messages");
-            const messageContent = escapeHtml(JSON.parse(xhr.response).text);
-            const message = `<article class="message"><p>${messageContent}</p></article>`;
-            messages.innerHTML += message;
-            messages.scrollTop = messages.scrollHeight;
-        }
-    };
-    const text = textBox.value;
-    textBox.value = "";
-    xhr.open("POST", `/messages?text=${text}`, true);
-    const token = getMetaContent("name", "_csrf");
-    const header = getMetaContent("name", "_csrf_header");
-    xhr.setRequestHeader(header, token);
-    xhr.send();
+const sendMessage = textBox => {
+	const text = textBox.value;
+	textBox.value = "";
+	request((response) => {
+		const messages = document.querySelector("section.messages");
+		const messageContent = escapeHtml(response.text);
+		const message = `<article class="message"><p>${messageContent}</p></article>`;
+		messages.innerHTML += message;
+		messages.scrollTop = messages.scrollHeight;
+	}, "POST", `/messages?text=${text}`);
 };
 
 const updateMessages = () => {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState === 4 && xhr.status === 200) {
-            const messages = document.querySelector("section.messages");
-            const response = JSON.parse(xhr.response);
-            messages.innerHTML = "";
-            for(let i = 0; i < response.length; i++) {
-                const text = escapeHtml(response[i].text);
-                const message = `<article class="message"><p>${text}</p></article>`;
-                messages.innerHTML += message;
-            }
-            messages.scrollTop = messages.scrollHeight;
-        }
-    };
-    xhr.open("GET", "/messages", true);
-    xhr.send();
+	request(response => {
+		const messages = document.querySelector("section.messages");
+		messages.innerHTML = "";
+		for (let i = 0; i < response.length; i++) {
+			const text = escapeHtml(response[i].text);
+			const message = `<article class="message"><p>${text}</p></article>`;
+			messages.innerHTML += message;
+		}
+		messages.scrollTop = messages.scrollHeight;
+	}, "GET", "/messages");
+};
+
+const request = (callback, method, url) => {
+	const xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = () => {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			const response = JSON.parse(xhr.response);
+			callback(response);
+		}
+	};
+	xhr.open(method, url, true);
+	const token = getMetaContent("name", "_csrf");
+	const header = getMetaContent("name", "_csrf_header");
+	xhr.setRequestHeader(header, token);
+	xhr.send();
 };
 
 const escapeHtml = unsafe => {
-    return unsafe
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
+	return unsafe
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#039;");
 };
 
-const getMetaContent = (property, name) => document.head.querySelector("["+property+"="+name+"]").content;
+const getMetaContent = (property, name) => document.head.querySelector("[" + property + "=" + name + "]").content;
