89cae557c4a5a67ebc2f682a0d89d17fe59ee267
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-05-30 14:36:00 -0400

    Changed SecurityController to send JSON

--- src/main/java/com/alexjamesmalcolm/persistmebaby/controller/SecurityController.java
+++ src/main/java/com/alexjamesmalcolm/persistmebaby/controller/SecurityController.java
@@ -4,15 +4,13 @@ import java.security.Principal;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecurityController {
 	
 	@RequestMapping(value = "/username", method = RequestMethod.GET)
-	@ResponseBody
-	public String currentUsername(Principal principal) {
-		return principal.getName();
+	public Principal currentUsername(Principal principal) {
+		return principal;
 	}
 }
