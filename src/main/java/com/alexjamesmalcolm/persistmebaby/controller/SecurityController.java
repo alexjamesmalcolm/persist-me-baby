dfdb98c5b395b1c0f808267562ddfb8500531dfb
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-05-30 14:46:44 -0400

    Switched to what baeldung called the 'simple version', probably doesn't work

--- src/main/java/com/alexjamesmalcolm/persistmebaby/controller/SecurityController.java
+++ src/main/java/com/alexjamesmalcolm/persistmebaby/controller/SecurityController.java
@@ -2,6 +2,8 @@ package com.alexjamesmalcolm.persistmebaby.controller;
 
 import java.security.Principal;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
 	
 	@RequestMapping(value = "/username", method = RequestMethod.GET)
-	public Principal currentUsername(Principal principal) {
+	public Principal currentUsername(HttpServletRequest request) {
+		Principal principal = request.getUserPrincipal();
 		return principal;
 	}
 }
