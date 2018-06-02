64453f1200920361f4ad3bbd9281f3a524fa328c
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-06-02 17:26:58 -0400

    Added user json data

--- src/main/java/com/alexjamesmalcolm/persistmebaby/controller/SecurityController.java
+++ src/main/java/com/alexjamesmalcolm/persistmebaby/controller/SecurityController.java
@@ -1,6 +1,7 @@
 package com.alexjamesmalcolm.persistmebaby.controller;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SecurityController {
 	
-	@RequestMapping(value = "/username", method = RequestMethod.GET)
-	public Authentication currentUsername(Authentication auth) {
+	@RequestMapping(value = "/auth", method = RequestMethod.GET)
+	public Authentication currentAuth(Authentication auth) {
 		return auth;
+	}
+	
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public CustomUser currentUser(@AuthenticationPrincipal CustomUser user) {
+		return user;
 	}
 }
