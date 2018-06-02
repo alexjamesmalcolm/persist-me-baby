98a103cf121c0d2ccf91c3d70a4c7a4ae3de7ae8
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-06-02 18:07:51 -0400

    Fleshed out CustomUser

--- src/main/java/com/alexjamesmalcolm/persistmebaby/CustomUser.java
+++ src/main/java/com/alexjamesmalcolm/persistmebaby/CustomUser.java
@@ -11,7 +11,7 @@ import com.alexjamesmalcolm.persistmebaby.message.Message;
 
 @Entity
 public class CustomUser {
-	
+
 	@Id
 	@GeneratedValue
 	private long id;
@@ -19,10 +19,30 @@ public class CustomUser {
 	@OneToMany(mappedBy = "user")
 	private Collection<Message> messages;
 	
+	private String fullName;
+	private String googleName;
+	
+	public CustomUser(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	@SuppressWarnings("unused")
 	private CustomUser() {}
+	
+	public String getFullName() {
+		return fullName;
+	}
+	
+	public void setGoogleName(String googleName) {
+		this.googleName = googleName;
+	}
+	
+	public String getGoogleName() {
+		return googleName;
+	}
 	
 	public Collection<Message> getMessages() {
 		return messages;
 	}
-
+	
 }
