14e951ec4958eb09c22df7547a0daa24d7508019
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-06-02 21:00:41 -0400

    Created method for finding users by their googleName

--- src/main/java/com/alexjamesmalcolm/persistmebaby/customuser/CustomUserRepository.java
+++ src/main/java/com/alexjamesmalcolm/persistmebaby/customuser/CustomUserRepository.java
@@ -3,5 +3,5 @@ package com.alexjamesmalcolm.persistmebaby.customuser;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CustomUserRepository extends CrudRepository<CustomUser, Long> {
-
+	CustomUser findByGoogleName(String googleName);
 }
