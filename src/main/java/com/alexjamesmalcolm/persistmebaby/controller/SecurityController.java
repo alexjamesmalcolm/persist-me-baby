bbacd3d19581d8ce0dbf916ea9f7082d7e8edef5
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2018-06-02 21:01:27 -0400

    Created mapping for retrieving the current user

--- src/main/java/com/alexjamesmalcolm/persistmebaby/controller/SecurityController.java
+++ src/main/java/com/alexjamesmalcolm/persistmebaby/controller/SecurityController.java
@@ -1,23 +1,39 @@
 package com.alexjamesmalcolm.persistmebaby.controller;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import java.security.Principal;
+
+import javax.annotation.Resource;
+
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alexjamesmalcolm.persistmebaby.CustomUser;
+import com.alexjamesmalcolm.persistmebaby.customuser.CustomUser;
+import com.alexjamesmalcolm.persistmebaby.customuser.CustomUserRepository;
 
 @RestController
 public class SecurityController {
 	
-	@RequestMapping(value = "/auth", method = RequestMethod.GET)
+	@Resource
+	private CustomUserRepository userRepo;
+	
+	@RequestMapping(value = "/principal", method = GET)
+	public Principal currentPrincipal(Principal principal) {
+		return principal;
+	}
+	
+	@RequestMapping(value = "/auth", method = GET)
 	public Authentication currentAuth(Authentication auth) {
 		return auth;
 	}
 	
-	@RequestMapping(value = "/user", method = RequestMethod.GET)
-	public CustomUser currentUser(@AuthenticationPrincipal CustomUser user) {
+	@RequestMapping(value = "/user", method = GET)
+	public CustomUser currentUser(Authentication auth) {
+		String name = auth.getName();
+		CustomUser user = userRepo.findByGoogleName(name);
 		return user;
 	}
+	
 }
